8a52099b10be826f3dc536088183d7b628cad73e
rocio@mumuki.org <rocio@mumuki.org>
2020-11-25 19:21:09 +0000

    Mumuki Export on 2020-11-25 19:21:09 +0000

--- 00018_¿Cuál elijo_/test.js
+++ 00018_¿Cuál elijo_/test.js
@@ -2,9 +2,9 @@ const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gob
             Match.aspectRatio = 2
             Match.strokeWidth = 6; // compensate the thinner stroke
             Match.match([
-              `${baseUrl}/match12_prog_no_1606331627470.svg`,
+              `${baseUrl}/match12_prog_si_1606331704226.svg`
             ], [
               `${baseUrl}/match12_tab_1606331726883.svg`,
             ], [
-              `${baseUrl}/match12_prog_si_1606331704226.svg`
+              `${baseUrl}/match12_prog_no_1606331627470.svg`,
             ], [], 2); // double the right pieces' width
