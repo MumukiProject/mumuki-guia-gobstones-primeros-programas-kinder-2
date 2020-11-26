2e8bdf271b8b27220799b5f9095edb4e187d293b
franco@mumuki.org <franco@mumuki.org>
2020-11-26 23:35:09 +0000

    Mumuki Export on 2020-11-26 23:35:08 +0000

--- 00018_¿Cuál elijo_/test.js
+++ 00018_¿Cuál elijo_/test.js
@@ -1,10 +1,11 @@
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-primeros-programas-kinder-2/master/assets/';
-            Muzzle.aspectRatio = 2
-            Muzzle.strokeWidth = 6; // compensate the thinner stroke
-            Muzzle.match([
-              `${baseUrl}/match12_prog_si_1606331704226.svg`
-            ], [
-              `${baseUrl}/match12_tab_1606331726883.svg`,
-            ], [
-              `${baseUrl}/match12_prog_no_1606331627470.svg`,
-            ], [], 2); // double the right pieces' width
+Muzzle.fitImagesVertically = true;
+Muzzle.aspectRatio = 2
+Muzzle.strokeWidth = 6; // compensate the thinner stroke
+Muzzle.match([
+  `${baseUrl}/match12_prog_si_1606331704226.svg`
+], [
+  `${baseUrl}/match12_tab_1606331726883.svg`,
+], [
+  `${baseUrl}/match12_prog_no_1606331627470.svg`,
+], [], 2); // double the right pieces' width
