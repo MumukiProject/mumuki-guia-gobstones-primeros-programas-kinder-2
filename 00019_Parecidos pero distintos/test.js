966c631d8dfc5cfa85e04b01ecaa35caa0f88917
rocio@mumuki.org <rocio@mumuki.org>
2020-12-04 15:07:40 +0000

    Mumuki Export on 2020-12-04 15:07:40 +0000

--- 00019_Parecidos pero distintos/test.js
+++ 00019_Parecidos pero distintos/test.js
@@ -1,4 +1,4 @@
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-primeros-programas-kinder-2/master/assets/';
-Muzzle.aspectRatio = 7 / 20
+Muzzle.aspectRatio = 4 / 10
 //Muzzle.strokeWidth = 6; 
 Muzzle.choose(`${baseUrl}/match13_prog_si_1606932027385.svg`, `${baseUrl}/match13_tab_b_1606932056965.svg`, [`${baseUrl}/match13_prog_no_1606931944432.svg`], 7 / 10);
