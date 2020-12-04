55315cdb328b59195b9bfcc3c995f13370e33787
rocio@mumuki.org <rocio@mumuki.org>
2020-12-04 14:58:53 +0000

    Mumuki Export on 2020-12-04 14:58:53 +0000

--- 00018_¿Cuál elijo_/test.js
+++ 00018_¿Cuál elijo_/test.js
@@ -1,11 +1,4 @@
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-primeros-programas-kinder-2/master/assets/';
-Muzzle.fitImagesVertically = true;
-Muzzle.aspectRatio = 2
-Muzzle.strokeWidth = 6; // compensate the thinner stroke
-Muzzle.match([
-  `${baseUrl}/match12_prog_si_1606331704226.svg`
-], [
-  `${baseUrl}/match12_tab_b_1606504604717.svg`,
-], [
-  `${baseUrl}/match12_prog_no_1606331627470.svg`,
-], [], 2); // double the right pieces' width
+Muzzle.aspectRatio = 0.5
+//Muzzle.strokeWidth = 6;
+Muzzle.choose(`${baseUrl}/match12_prog_si_1606331704226.svg` , `${baseUrl}/match12_tab_b_1606504604717.svg`, [`${baseUrl}/match12_prog_no_1606331627470.svg`], 1);
